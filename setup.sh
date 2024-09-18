6da8cec6fe65836ae910e75a731b891e2e7a390d
indocoding-ID <indocoding.id@gmail.com>
2024-09-19 05:14:03 +0700

    Update setup.sh
    add build-essential

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ pkg update -y
 pkg upgrade -y
 
 # install cmake dan golang
-pkg install git cmake golang -y
+pkg install git cmake build-essential golang -y
 
 # goto root
 cd ~/
@@ -27,17 +27,17 @@ chmod -R 700 ~/go
 rm -r ~/go
 
 # set up bin ollama
-cp ~/ollama/ollama /data/data/com.termux/files/usr/bin/
+cp ~/ollama/ollama $PREFIX/bin/
 
 # create started
-cat << EOF > /data/data/com.termux/files/usr/bin/startollama
+cat << EOF > $PREFIX/bin/startollama
 #!/bin/sh
 export OLLAMA_HOST=0.0.0.0:11434
 export OLLAMA_ORIGINS='*'
 nohup ollama serve > /dev/null 2>&1&
 EOF
 
-chmod +x /data/data/com.termux/files/usr/bin/startollama
+chmod +x $PREFIX/bin/startollama
 
 echo "Sekarang anda bisa menjalankan ollama dengan startollama dan cek ollama dengan ollama list";
 echo "install module ollama yang anda inginkan"
